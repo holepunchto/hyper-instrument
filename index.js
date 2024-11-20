7f059c5864cd56f1d6a61e4d1ea0b7929b47e60c
HDegroote <75906619+HDegroote@users.noreply.github.com>
2024-11-20 16:07:21 +0100

    Basic instrumentation

---
+++ index.js
@@ -0,0 +1,66 @@
+const path = require('path')
+const DhtPromClient = require('dht-prom-client')
+const HyperswarmStats = require('hyperswarm-stats')
+const HypercoreStats = require('hypercore-stats')
+const promClient = require('prom-client')
+
+// Attempt to get the package version of the main module (commonJS only)
+let PACKAGE_VERSION = null
+try {
+  const loc = path.join(require.main.path, 'package.json')
+  console.log(require.main, loc)
+  const { version } = require(loc)
+  PACKAGE_VERSION = version
+} catch {} // could not extract version
+
+function hyperInstrument ({
+  swarm,
+  corestore,
+  dht,
+  prometheusScraperPublicKey,
+  prometheusAlias,
+  prometheusSecret,
+  prometheusServiceName,
+  logger
+}) {
+  if (swarm && dht) throw new Error('Exactly 1 of dht or swarm should be specified')
+  if (swarm) dht = swarm.dht
+
+  promClient.collectDefaultMetrics()
+  if (PACKAGE_VERSION) registerPackageVersion(PACKAGE_VERSION)
+
+  const swarmStats = new HyperswarmStats(swarm)
+  swarmStats.registerPrometheusMetrics(promClient)
+
+  const hypercoreStats = HypercoreStats.fromCorestore(corestore)
+  hypercoreStats.registerPrometheusMetrics(promClient)
+
+  const promRpcClient = new DhtPromClient(
+    dht,
+    promClient,
+    prometheusScraperPublicKey,
+    prometheusAlias,
+    prometheusSecret,
+    prometheusServiceName
+  )
+
+  if (logger) promRpcClient.registerLogger(logger)
+
+  return promRpcClient
+}
+
+function registerPackageVersion (version) {
+  // Gauges expect a number, so we set the version as label instead
+  return new promClient.Gauge({
+    name: 'package_version',
+    help: 'Package version in config.json',
+    labelNames: ['version'],
+    collect () {
+      this.labels(
+        version
+      ).set(1)
+    }
+  })
+}
+
+module.exports = hyperInstrument
